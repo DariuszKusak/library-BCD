bf1bea55169f3f58df53aa9382cee813b91012ca
DariuszKusak <dariusz.kusak@gmail.com>
2020-02-12 21:03:09 +0100

    Repo started

---
+++ src/main/java/com/bcd/library/librarybcd/LibraryBcdApplication.java
@@ -0,0 +1,13 @@
+package com.bcd.library.librarybcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBcdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcd/library/librarybcd/LibraryBcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcd.library.librarybcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBcdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
